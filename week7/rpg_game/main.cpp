8331cf003211e30051329e09d1f1230a4d2859ef
Oszvaldb <nagyongonoszi@gmail.com>
2016-12-07 18:02:30 +0100

    new feat: moves, but does not turn

--- week7/rpg_game/main.cpp
+++ week7/rpg_game/main.cpp
@@ -4,28 +4,51 @@
 #include "level_builder.hpp"
 
 bool level [10] [10];
-
+int x = 0;
+int y = 0;
 class MyGame : public Game {
 public:
   MyGame() {}
   virtual void init(GameContext& context) {
     context.load_file("floor.bmp");
     context.load_file("wall.bmp");
+    context.load_file("hero-down.bmp");
+    context.load_file("hero-up.bmp");
+    context.load_file("hero-right.bmp");
+    context.load_file("hero-left.bmp");
   }
   virtual void render(GameContext& context) {
     for (int i = 0; i < 10; i ++){
     	for (int j = 0; j < 10; j ++) {
     		if (level[i] [j] == true) {
     		context.draw_sprite("floor.bmp", i * 72, j * 72);
-    		    context.render();
     		} else {
     			context.draw_sprite("wall.bmp", i * 72, j * 72);
-    			    context.render();
     		}
     	}
     }
-  //context.draw_sprite("wall.bmp", 72, 72);
-  	//context.render();
+		context.draw_sprite("hero-down.bmp", x * 72, y * 72);
+		//context.render();
+		if (context.was_key_pressed(ARROW_DOWN)) {
+			context.draw_sprite("hero-down.bmp", x * 72, y * 72);
+			if(level [x] [y+1] == true && (y + 1) * 72 < 720) {
+				y++;
+				context.draw_sprite("hero-down.bmp", x * 72, y * 72);
+				//context.render();
+			}
+			context.render();
+			context.reset_keys();
+		}else if (context.was_key_pressed(ARROW_RIGHT)) {
+			context.draw_sprite("hero-right.bmp", x * 72, y * 72);
+			if(level [x+1] [y] == true && (x + 1) * 72 < 720) {
+				x++;
+				context.draw_sprite("hero-right.bmp", x * 72, y * 72);
+				//context.render();
+			}
+			context.render();
+			context.reset_keys();
+		}
+		context.render();
   }
 };
 
